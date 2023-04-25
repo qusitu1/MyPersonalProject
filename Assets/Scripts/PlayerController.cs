c79a38ba779148706e17fa2c578f598cf67e9893
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-04-25 10:29:26 -0700

    initial commit
    horizontal movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody rb;
-
-    private float forwardForce = 50f;
-    public float sidewaysForce = 50f;
+    public float speed = 10f;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,30 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-
-        if(Input.GetKey("d"))
-        {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if(Input.GetKey("a"))
-        {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if(Input.GetKey("w"))
-        {
-            rb.AddForce(forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if(Input.GetKey("s"))
-        {
-            rb.AddForce(-forwardForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-    
+        MovePlayer();
     }
 
-}
+    //Move player based on WASD input
+    void MovePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+    }
+} 
+
 

be3033fad3910fbd849a977882c5f4a9f655c7d5
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-05-16 10:32:36 -0700

    initial commit
    made first checkpoint

---
+++ Assets/Scripts/CheckSpawnPoint.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckSpawnPoint : MonoBehaviour
+{
+    public string identifier;
+    public GameObject objectToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //Player has reached the check/spawn point
+            Debug.Log("Player reached checkpoint: " + identifier);
+        }
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(objectToSpawn, transform.position, transform.rotation);
+    }
+}

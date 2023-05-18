bae80e5cfddfc03e89f03255340225e7b5f2a6d0
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2023-05-18 12:39:02 -0700

    Initial commit

--- Assets/Scripts/UITimeDisplay.cs
+++ Assets/Scripts/UITimeDisplay.cs
@@ -7,7 +7,8 @@ using UnityEngine.UI;
 public class UITimeDisplay : MonoBehaviour
 {
     public TimeLimitController timeLimitController;
-    private Text timeText;
+    public Text timeText;
+
     // Start is called before the first frame update
     void Start()
     {
